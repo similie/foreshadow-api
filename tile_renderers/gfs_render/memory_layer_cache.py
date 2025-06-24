a0f504b1a9b1138bd32e80f571ee7966271a47de
Adam Smith <adam.smith@similie.org>
2025-06-24 17:34:07 +0800

    attempting to fix crashing threads

--- tile_renderers/gfs_render/memory_layer_cache.py
+++ tile_renderers/gfs_render/memory_layer_cache.py
@@ -574,7 +574,7 @@ class MemoryLayerCache:
         print("Preloader finished")
         self._init_run = False
 
-    def preloader(self):
+    async def preloader(self):
         try:
             self.preload_slices()
             self.preload_tiles()
--- tile_renderers/main.py
+++ tile_renderers/main.py
@@ -350,13 +350,14 @@ async def _prewarm_loop(
             print("PRELOAD EXECUTION STARTED")
             try:
                 if last_future is None or last_future.done():
-                    loop = asyncio.get_running_loop()
+                    # loop = asyncio.get_running_loop()
                     logger.info("Submitting new prewarm task")
                     # last_future =  prewarm_process_executor.submit(run_workers)
-                    last_future = loop.run_in_executor(
-                        None,
-                        layer_cache.preloader #layer_cache.preload_to_local()
-                    )
+                    await layer_cache.preloader()
+                    # last_future = loop.run_in_executor(
+                    #     None,
+                    #     layer_cache.preloader #layer_cache.preload_to_local()
+                    # )
                     # last_future = app.state.prewarm_executor.submit(
                     #     layer_cache.preloader #layer_cache.preload_to_local()
                     # )
