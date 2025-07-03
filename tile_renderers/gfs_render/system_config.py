89c056107ebff10dcf07e5129cf20cd5ddedec9a
Adam Smith <adam.smith@similie.org>
2025-07-03 14:34:37 +0900

    chore: added cloud cover

--- tile_renderers/gfs_render/system_config.py
+++ tile_renderers/gfs_render/system_config.py
@@ -34,6 +34,11 @@ class SystemConfig:
         data = '''{
             "model": "gfs",
             "param_keys": [{
+                  param_key: "total-cloud-cover",
+                  level: 0,
+                  typeOfLevel: "atmosphere",
+                  stepType: "avg"
+                },{
                 "param_key": "surface-pressure",
                 "typeOfLevel": "surface",
                 "level": 0,
